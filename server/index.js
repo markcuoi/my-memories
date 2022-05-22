6b50f45f425562ddbecec7f51da32dc8c8c3c71a
markshiba <thanhphan10397@gmail.com>
2022-05-22 15:17:04 +0700

    update eslint

--- server/index.js
+++ server/index.js
@@ -23,7 +23,9 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
   .catch((error) => {
     console.log(error.message);
   });
